5ba68a8323a01f217246a90ce37389edd10c4fea
lukaseriksson <43849019+lukaseriksson@users.noreply.github.com>
2020-04-28 11:15:26 +0200

    Add files via upload

---
+++ src/chat/client.java
@@ -0,0 +1,9 @@
+package chat;
+
+import javax.swing.*;
+
+public class client {
+    private JButton msg_send;
+    private JTextArea msg_area;
+    private JTextField msg_text;
+}
---
+++ src/chat/server.java
@@ -0,0 +1,10 @@
+package chat;
+
+import javax.swing.*;
+
+public class server {
+    private JPanel panel1;
+    private JTextField msg_area;
+    private JTextField msg_text;
+    private JButton msg_send;
+}
